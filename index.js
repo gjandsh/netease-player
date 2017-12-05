06e4f72a51d3f2c1c9c0d2e03ce8ed203e4b82ea
gjandsh <gjandsh@126.com>
2017-12-05 19:21:40 +0800

    添加首页内容

---
+++ index.js
@@ -0,0 +1,100 @@
+$(function() {
+    $.get('./songs.json').then(function(response) {
+      let items = response
+      items.forEach(i => {
+        let $li = $(
+          `
+					<li>
+					<a href="./song.html?id=${i.id}">
+					<h3>${i.name}</h3>
+					<p>${i.singer}</p>
+					<svg class="play">
+					<use xlink:href="#icon-weibiaoti1"></use>
+					</svg>
+					</a>
+					</li>
+					`
+        )
+        $('#lastestMusic').append($li)
+      })
+      $('#lastestMusicLoading').remove()
+    }) 
+
+  $('.siteNav').on('click', 'ol.tabItems>li', function(e) {
+    let $li = $(e.currentTarget).addClass('active')
+    $li.siblings().removeClass('active')
+    let index = $li.index()
+    $li.trigger('tabChange', index)
+    $('.tabContent > li').eq(index).addClass('active').siblings().removeClass('active')
+  })
+
+  $('.siteNav').on('tabChange', function(e, index) {
+    let $li = $('.tabContent > li').eq(index)
+    if($li.attr('data-downloaded') === 'yes'){
+      return 
+    }
+    setTimeout(function(){
+      if (index === 1) {
+        $.get('./page2.json').then((response)=>{
+          $li.text(response.content)
+          $li.attr('data-downloaded', 'yes')
+        })
+      }else if(index === 2){
+        return
+        $.get('./page3.json').then((response)=>{
+          $li.text(response.content)
+          $li.attr('data-downloaded', 'yes')
+        })
+      }
+    }, 500)
+  })
+
+  let timer = undefined
+  $('input#searchSong').on('input', function(e){
+    let $input = $(e.currentTarget)
+    let value = $input.val().trim()
+    if(value === ''){return}
+
+    if(timer){
+      clearTimeout(timer)
+    }
+
+    timer = setTimeout(function(){
+      search(value).then((result)=>{
+        timer = undefined
+        if(result.length !== 0){
+          $('#output').empty()
+          let $ul = $('<ul></ul>')
+          result.forEach((item)=>{
+            let $li = $(`<li><a href="/song.html?id=${item.id}">${item.name}</a></li>`)
+            $li.appendTo($ul)
+          })
+          $('#output').append($ul)
+        }else{
+          $('#output').html('没有结果')
+        }
+      })
+    },300)
+
+
+  })
+
+  function search(keyword){
+    console.log('搜索'+keyword)
+    return new Promise((resolve, reject)=>{
+      var database = [
+        { "id": 1, "name":"那些花儿", },
+        { "id": 2, "name":"情非得已", },
+        { "id": 3, "name":"找自己", },
+      ]
+      let result = database.filter(function(item){
+        return item.name.indexOf(keyword)>=0
+      })
+      setTimeout(function(){
+        console.log('搜到'+keyword+ '的结果')
+        resolve(result)
+    }, (Math.random()*200 + 1000))
+    })
+  }
+  window.search = search
+})
--- song.js
+++ song.js
@@ -1,72 +1,85 @@
-$(function(){
-	$.get('./lyric.json').then(function(object){
-		//console.log(object)
-		let {lyric} = object // 等同于lyric = object.lyric
+$(function () { // $(callback)监听DOM加载完成
+	let songid = location.search.match(/\bid=([^&]*)/)[1];
+	$.get('./songs.json').then(function (object) {
+		console.log(songid)
+		let {
+			singer,
+			lyric,
+			url
+		} = object[songid] // 等同于lyric = object.lyric
 		let array = lyric.split('\n')
 		//console.log(array)
 		let regex = /^\[(.+)\](.*)$/
-		array = array.map(function(string, index){
-			let matches = string.match(regex)	
-		//	console.log(matches)
-			if(matches){
-				return {time: matches[1], words: matches[2]}
+		array = array.map(function (string, index) {
+			let matches = string.match(regex)
+			//	console.log(matches)
+			if (matches) {
+				return {
+					time: matches[1],
+					words: matches[2]
+				}
 			}
 		})
 		let $lyric = $('.lyric')
-		array.map(function(object){
-			if(!object){return}
+		array.map(function (object) {
+			if (!object) {
+				return
+			}
 			let $p = $('<p/>')
 			$p.attr('data-time', object.time).text(object.words)
 			$p.appendTo($lyric.children('.lines'))
 		})
+
+		let audio = document.createElement('audio')
+		audio.src = url
+		audio.oncanplay = function () {
+			audio.play()
+			$('.disc-container').addClass('playing')
+			$('.pointer').removeClass('leaveDisc')
+		}
+		$('.icon-pause').on('click', function () {
+			audio.pause()
+			$('.disc-container').removeClass('playing')
+			$('.pointer').addClass('leaveDisc')
+
+		})
+		$('.icon-play').on('click', function () {
+			audio.play()
+			$('.disc-container').addClass('playing')
+			$('.pointer').removeClass('leaveDisc')
+		})
+		let $title = $('.song-description>h1')
+		console.log(singer.split('-'))
+		$title.text(singer)
+		setInterval(() => {
+			let seconds = audio.currentTime
+			let minutes = ~~(seconds / 60)
+			let left = seconds - minutes * 60
+			let time = `${pad(minutes)}:${pad(left)}`
+			//console.log(time)
+			let $lines = $('.lines>p')
+			let $whichLine
+			//console.log($lines)
+			for (let i = 0; i < $lines.length; i++) {
+				if ($lines[i + 1] !== undefined && $lines.eq(i).attr('data-time') < time && $lines.eq(i + 1).attr('data-time') > time) {
+					$whichLine = $lines.eq(i)
+					break
+				}
+			}
+			if ($whichLine) {
+				$whichLine.addClass('active').prev().removeClass('active')
+
+				let top = $whichLine.offset().top
+				let linesTop = $('.lines').offset().top
+				let delta = top - linesTop - $('.lyric').height() / 3
+				$('.lines').css('transform', `translateY(-${delta}px)`)
+
+
+			}
+		}, 300)
 	})
 
-	let audio = document.createElement('audio')
-	audio.src = '//os5kf123g.bkt.clouddn.com/2d15%252Fd9fd%252F57cd%252F6679396a63ff496ecef0453b25612dfa.mp3'
-	audio.oncanplay = function(){
-		audio.play()
-		$('.disc-container').addClass('playing')
-		$('.pointer').removeClass('leaveDisc')
-	}	
-	$('.icon-pause').on('click', function(){
-		audio.pause()	
-		$('.disc-container').removeClass('playing')
-		$('.pointer').addClass('leaveDisc')
-		
-	})
-	$('.icon-play').on('click', function(){
-		audio.play()	
-		$('.disc-container').addClass('playing')
-	    $('.pointer').removeClass('leaveDisc')
-	})
-	setInterval(()=>{
-	let seconds = audio.currentTime
-	let minutes = ~~(seconds/60)
-	let left = seconds - minutes*60
-	let time = `${pad(minutes)}:${pad(left)}`
-	//console.log(time)
-        let $lines = $('.lines>p')
-	let $whichLine
-		//console.log($lines)
-	for(let i=0; i<$lines.length; i++){
-	  if($lines[i+1]!==undefined&&$lines.eq(i).attr('data-time')<time&&$lines.eq(i+1).attr('data-time')>time){
-	  $whichLine = $lines.eq(i)
-          break}
-	}
-	  if($whichLine){
-             $whichLine.addClass('active').prev().removeClass('active')
-
-	     let top = $whichLine.offset().top
-             let linesTop = $('.lines').offset().top
-	     let delta = top - linesTop - $('.lyric').height()/3
-	     $('.lines').css('transform',`translateY(-${delta}px)`)
-	  
-	  
-	  }	
-	}
-		,300)
-
-	function pad(number){
-	return number>=10 ? number + '': '0'+number
+	function pad(number) {
+		return number >= 10 ? number + '' : '0' + number
 	}
 })
